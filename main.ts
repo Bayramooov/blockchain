7efc85788ec25f584c7510f3cc2c675e2085b798
Sardor Bayramov <BayramovSardor@gmail.com>
2022-01-20 02:17:19 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,42 @@
+class User {
+  public_key: string;
+  secret_key: string;
+
+  generateKey(password: string): void {
+    this.public_key = password;
+    this.secret_key = password.substr(-3);
+  }
+}
+
+
+class Data {
+  hash: string;
+}
+
+
+class Block {
+  hash: string;
+  prev_hash: string;
+
+  from: User;
+  signature: string;
+
+  // Signature
+  sign(secret_key: string): void {
+    this.signature = `${this.hash}/${this.data.hash}/${secret_key}`;
+    console.log(this.signature);
+  }
+
+  // Verification
+  verify(public_key: string): boolean {
+    let signature = `${this.hash}/${this.data.hash}/${public_key.substr(-3)}`;
+    if (this.signature === signature) return true;
+    else return false;
+  }
+}
+
+
+class Miner {
+  name: string;
+  coin: number;
+}
